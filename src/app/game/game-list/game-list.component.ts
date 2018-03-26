a85322ec6ead05984d0375f34f5a7f50c5f2a4a1
doraeball22 <anurak_b@cmu.ac.th>
2018-03-26 17:10:18 +0700

    updated

--- src/app/game/game-list/game-list.component.ts
+++ src/app/game/game-list/game-list.component.ts
@@ -4,6 +4,7 @@ import { SafeResourceUrl, DomSanitizer } from '@angular/platform-browser';
 
 import { Game } from '../shared/game';
 import { GAMES } from '../shared/mock-games';
+import { forEach } from '@angular/router/src/utils/collection';
 
 @Component({
   selector: 'app-game-list',
@@ -14,6 +15,8 @@ export class GameListComponent implements OnInit {
 
   games: Game[] = GAMES;
   selectedGame: Game;
+
+  gameYoutubeUrls: any = [];
 
   video: any = {id: 'wzrnuUOoFNM'};
   baseUrl:string = 'https://www.youtube.com/embed/';
@@ -26,11 +29,19 @@ export class GameListComponent implements OnInit {
   ngOnInit() {
     // this.url = this.sanitizer.bypassSecurityTrustResourceUrl(""+this.games[1].youtubeEmbed);
     this.onSelect(this.games[0]);
+
+    this.byPassUrl();
   }
 
   onSelect(game: Game): void {
     this.selectedGame = game;
     this.url = this.sanitizer.bypassSecurityTrustResourceUrl(""+this.selectedGame.youtubeEmbed);
+  }
+
+  private byPassUrl() {
+    for(let game of this.games) {
+      this.gameYoutubeUrls.push(this.sanitizer.bypassSecurityTrustResourceUrl(""+game.youtubeEmbed));
+    }
   }
 
 
